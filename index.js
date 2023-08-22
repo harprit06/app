639cbbd345a3d21e87dab24ba1d4c228c12b826c
harprit06 <111792663+harprit06@users.noreply.github.com>
2023-08-23 01:33:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Taru!\n'
+  const msg = 'Hello All! This Site is just for trial basis , Kindly don't be tense and just chill!\n'
   res.end(msg);
 });
 
