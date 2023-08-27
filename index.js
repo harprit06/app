dcad04f9163af5fa62592a9acf7f491eacb77fef
harprit06 <111792663+harprit06@users.noreply.github.com>
2023-08-27 16:42:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello All My Mates!\n'
+  const msg = 'Hello Nodes!, This is a Node.js application\n'
   res.end(msg);
 });
 
